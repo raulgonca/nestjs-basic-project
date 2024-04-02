92af8d018dcbb3657afc4690d1bb39895e4fb9f9
raulgonca <raulgo2003@gmail.com>
2024-04-02 14:07:47 +0200

    Martes

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,19 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cors from 'cors';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
- 
 
   app.use(cors({
     origin: 'http://localhost:5173/',
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Other-Custom-Header'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   }));
-  
 
   await app.listen(3000);
-
 }
 
 bootstrap();
